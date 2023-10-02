18ab76a0c5eeeafc4af8bced02394538174cf16d
mousamsangwanXD <97466408+mousamsangwanXD@users.noreply.github.com>
2023-10-02 15:38:11 +0000

    Modified Line-Height of Heading, paragraph text, and button to free up some space

--- components/GetWhatsapp.tsx
+++ components/GetWhatsapp.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const GetWhatsapp = () => {
   return (
-    <div className="flex flex-col h-full items-center justify-center relative py-7">
-      <div className="mt-[-20px] w-[80%] max-w-[560px]">
+    <div className="flex h-full items-center justify-center py-7">
+      <div className="w-[80%] max-w-[560px] text-center mt-[-20px]">
         <Image
           src="/getWhatsapp.png"
           alt="GetWhatsApp"
@@ -12,31 +12,28 @@ const GetWhatsapp = () => {
           height={188}
           className="mx-auto"
         />
-        <div className="text-center">
-          <h2 className="text-[32px] text-[#e9edef]/[0.88] font-light mt-7">
+          <h2 className="text-[32px] text-[#e9edef]/[0.88] leading-8 font-light mt-7">
             Download WhatsApp for Windows
           </h2>
-          <p className="text-[#8696a0] text-[14px] mt-[18px]">
+          <p className="text-[#8696a0] text-[14px] leading-5 mt-[18px]">
             Make calls, share your screen and get a faster experience when you
             download the Windows app.
           </p>
           <div className="mt-8">
-            <button className="border border-[#111b21] bg-[#00a884] rounded-3xl text-[#111b21] text-[14px] font-medium py-2.5 px-6 ">
+            <button className="border border-[#00a884] bg-[#00a884] rounded-3xl text-[#111b21] text-[14px] leading-4 font-medium py-2.5 px-6 ">
               Get the app
             </button>
           </div>
-        </div>
-      </div>
-      <div className="flex gap-1 absolute bottom-10 justify-center">
+        <div className="absolute left-0 right-0 bottom-10 text-sm text-[#667781]">
         <Image
           src="/privacyLock.svg"
           alt="Privacy Policy"
           width={10}
           height={12}
+          className="inline-block mb-[2px]"
         />
-        <p className="text-[#667781] text-[14px]">
-          Your personal messages are end-to-end encrypted
-        </p>
+        <span> Your personal messages are end-to-end encrypted</span>
+      </div>
       </div>
     </div>
   );
